ce1e5206789af07d1b782baff49ffd336c031423
ojcarlos <65193386+ojcarlos@users.noreply.github.com>
2021-11-07 17:50:15 -0400

    Add files via upload

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,41 @@
+import RepositoryItem from "./RepositoryItem";
+import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
+
+interface Repository{
+    name: string;
+    description: string;
+    html_url: string;
+}
+
+//https://api.github.com/users/ojcarlos/repos
+
+const repository = {
+    name: 'unform',
+    description: 'form in react',
+    link: 'https://github.com/'
+}
+
+
+export function RepositoryList(){
+
+    const [repositories, setRepositories] = useState<Repository[]> ([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/ojcarlos/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    },[])
+
+    return (
+        <section className="repository-list" >
+            <h1>Lista de repositorios!</h1>
+
+            <ul>
+                {repositories.map(repository =>{
+                    return <RepositoryItem key={repository.name} repository = { repository} />
+                } )}
+            </ul>
+        </section>
+    )
+}
